bbe71779c89add48a50348610a2cc8d2e9b3acd1
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2026-05-20 13:48:10 +0530

    file name correct

--- src/Views/Admin/admin_dashboard.jsx
+++ src/Views/Admin/admin_dashboard.jsx
@@ -136,9 +136,7 @@ export default function AdminDashboard() {
                             </div>
                         </div>
                 </main>
-
-       
-       </>
+                </>
     )
 
 };
