74a41fb768b919743eb35cac843ab0778eaab61f
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2026-05-20 14:28:47 +0530

    file rename

--- src/Services/routes_services.jsx
+++ src/Services/routes_services.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router";
 import { UserData } from "../Controllers/authController"
 import { Route } from "lucide-react";
-import Settings from "../Views/Admin/Settings/settings";
+import Settings from "../Views/Admin/Settings/Settings";
 
 export const RouteServices = {
     home : "/",
