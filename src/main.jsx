e76c9a710eb3fc8886b1a3f73437a9a310158b43
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2026-05-20 14:34:51 +0530

    re-merge files

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppDataProvider } from './Controllers/DataController/ProjectCardData.jsx'
-import { UserLoginData } from './Controllers/AuthController.jsx'
-import { CategoryDataProvider } from './Controllers/DataController/Add_Category.jsx'
+import { AppDataProvider } from './Controllers/DataController/projectCardData.jsx'
+import { UserLoginData } from './Controllers/authController.jsx'
+import { CategoryDataProvider } from './Controllers/DataController/add_Category.jsx'
 // import {BrowserRouter} from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
